e1c570bb85831260f7ea9580ccb7d2310f115050
Ruggero Pintus <ruggero.pintus@gmail.com>
2022-02-22 14:31:55 +0100

    Start testing camera calibration procedure (WIP)

--- src/exe_sandbox/main_sandbox.cpp
+++ src/exe_sandbox/main_sandbox.cpp
@@ -1,1 +1,46 @@
-int main(int /*argc*/, char** /*argv*/) { return 0; }
+#include <cstdlib>
+#include <opencv2/core.hpp>
+#include <string>
+#include <vector>
+
+struct ChessboardData {
+	cv::Size size;
+	float squareSize;
+	std::size_t viewCount;
+};
+
+std::vector<std::vector<cv::Vec3f>> computeObjectPoints(
+	const ChessboardData& chessboardData);
+
+int main(int argc, char** argv) {
+	ChessboardData chessboardData;
+
+	chessboardData.size = cv::Size(std::atoi(argv[1]), std::atoi(argv[2]));
+	chessboardData.squareSize = std::atof(argv[3]);
+	chessboardData.viewCount = argc - 4;
+
+	std::vector<std::vector<cv::Vec3f>> objectPoints =
+		computeObjectPoints(chessboardData);
+
+	// std::vector<std::string> chessboardCornersFNames(argc - );
+
+	return 0;
+}
+
+std::vector<std::vector<cv::Vec3f>> computeObjectPoints(
+	const ChessboardData& /*chessboardData*/) {
+	// TODO: implement me
+	return std::vector<std::vector<cv::Vec3f>>();
+}
+
+// double 	cv::calibrateCamera
+
+// double 	cv::calibrateCamera
+
+// double 	cv::calibrateCameraRO (
+
+// void 	cv::calibrationMatrixValues
+
+// void 	cv::drawFrameAxes
+
+// void 	cv::projectPoints
